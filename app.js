e6ec4d5c330c8e9badb1f0b6c38f5686cad1fd96
Javi Plaza <javierplazasisques@gmail.com>
2021-12-05 20:35:12 +0100

    Root commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+//Settings
+app.set('port', 8080);
+app.set('views', path.join(__dirname + '/server/views'));
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'ejs');
+
+//Routes
+//app.use(require('./server/microservices/upload_photo/routes'))
+//app.use(require('./server/microservices/photo/routes'))
+
+// Starting server at 8080
+app.listen(app.get('port'), function () {
+    console.log('Servidor iniciado en http://localhost:' + app.get('port'))
+  })
